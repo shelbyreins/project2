e0e09384d98e647cdc7c12df7cf1bf0fd44b6563
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-01-13 21:41:55 -0800

    set up express and port on server.js and set up routes

---
+++ app/routes/api-routes.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var router = express.Router();
+
+router.get("/api/log", function(req, res) {
+
+});
+
+router.post("/api/log", function(req, res) {
+
+});
+
+router.put("/api/log/:id", function(req, res) {
+
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 var express = require("express");
+
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+
+
+var htmlRoutes = require("./app/routes/html-routes");
+app.use(htmlRoutes);
+
+var apiRoutes = require("./app/routes/api-routes");
+app.use(apiRoutes);
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
