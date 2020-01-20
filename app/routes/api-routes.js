0c9a2b4156944c5a64bb297e665b4f9d7ab11e1c
dimple shanbhag <dimpleshanbhag@dimples-MacBook-Pro.local>
2020-01-20 13:48:36 -0800

    sign in data with user id is fetched from db

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -13,6 +13,17 @@ router.post("/api/signup", function(req, res) {
   db.User.create(req.body).then(data=>res.send(data))
 });
 
+router.get("/api/signin/:username", function(req, res) {
+  console.log("Checking DB for user....")
+  db.User.findAll({
+    where: {
+      username:req.params.username
+    }
+  }).then(function(results){
+    console.log(res.json(results));
+  })
+});
+
 router.put("/api/log/:id", function(req, res) {
 
 });
