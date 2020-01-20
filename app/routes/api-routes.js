1e2e56899c566ae188c554e26927b221a7ff0d29
dimple shanbhag <dimpleshanbhag@dimples-MacBook-Pro.local>
2020-01-20 09:54:05 -0800

    working sequelize

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var db = require('../models');
 
 var router = express.Router();
 
@@ -7,8 +8,8 @@ router.get("/api/log", function(req, res) {
 
 });
 
-router.post("/api/log", function(req, res) {
-
+router.post("/api/signup", function(req, res) {
+  db.User.create(req.body).then(data=>console.log(data))
 });
 
 router.put("/api/log/:id", function(req, res) {
