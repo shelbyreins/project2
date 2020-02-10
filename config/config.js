db573ecfd1f4f4c59be4a66d275f2c5cd04537f1
Shelby Reinsimar <shelbyreins@yahoo.com>
2020-02-10 08:46:35 -0800

    Set up config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,12 @@
-{
+require('dotenv').config();
+
+ module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
+    "dialect": "mysql"
   },
   "test": {
     "username": "root",
@@ -16,11 +17,7 @@
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
   }
 }
